bd9346cff2230f00dc71e33273f29476a5e1b262
AlexAksioma <alexmedqwerty@gmail.com>
2024-04-15 17:30:02 +0300

    created basetest, userdto

---
+++ src/main/java/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@ToString
+@Getter
+@Setter
+public class UserDTO {
+    private String email;
+    private String password;
+}
